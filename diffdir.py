acb8c338a6c255109b576995445b9b2fe2f56796
zhaojyu <zhaojyu@163.com>
2025-09-16 17:34:21 +0800

    完成图片替换脚本，抽取公用方法

--- diffdir.py
+++ diffdir.py
@@ -1,9 +1,12 @@
+import hashlib
+import json
 import os
 import shutil
-from filecmp import cmp
+from pathlib import Path
 from typing import Callable
 
-def file_copy(file_path:str,target_path:str)->None:
+
+def file_copy(file_path: str, target_path: str) -> None:
     """
     文件拷贝
     :param file_path: 文件路径
@@ -16,14 +19,79 @@ def file_copy(file_path:str,target_path:str)->None:
         return
     if not os.path.exists(target_path):
         # 创建目录
-        os.mkdir(target_path)
+        os.makedirs(target_path)
     shutil.copy2(file_path, target_path)
 
 
-def path_compare(root_path: str, target_path: str, file_same_call: Callable[[str, str], None] = None,
-                 file_diff_call: Callable[[str, str], None] = None, file_miss_call: Callable[[str], None] = None)->None:
+def read_json_file(file_path: str):
+    """
+    读取json文件
+    :param file_path: 文件路径
+    :return: None
+    """
+    if not os.path.isfile(file_path):
+        return None
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            return data
+    except json.JSONDecodeError:
+        return None
+
+
+def write_json_to_file(data, file_path) -> None:
+    """
+    写入json文件
+    :param data: json数据
+    :param file_path: 写入的文件路径
+    :return: None
+    """
+    try:
+        with open(file_path, 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+        print(f"JSON 数据已写入文件: {file_path}")
+    except Exception as e:
+        print(f"写入文件失败: {e}")
+
+
+def get_file_md5(file_path: str) -> str:
+    """
+    获取文件md5
+    :param file_path: 文件路径
+    :return:
+    """
+    md5_hash = hashlib.md5()
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            md5_hash.update(chunk)
+    return md5_hash.hexdigest()
+
+
+def compare_files(root_path: str, target_path: str, part_suffix: list = None) -> bool:
+    """
+    对比两个文件
+    :param root_path: 输入文件
+    :param target_path: 对比文件
+    :param part_suffix: 参与比对的文件后缀
+    :return:bool
+    """
+    if part_suffix is None:
+        part_suffix = []
+    if (os.path.basename(root_path).split('.')[-1] in part_suffix and
+            os.path.basename(target_path).split('.')[-1] in part_suffix):
+        md5_1 = get_file_md5(root_path)
+        md5_2 = get_file_md5(target_path)
+        return md5_1 == md5_2
+    return True
+
+
+def path_compare(root_path: str, target_path: str, part_suffix: list = None,
+                 file_same_call: Callable[[str, str], None] = None,
+                 file_diff_call: Callable[[str, str], None] = None,
+                 file_miss_call: Callable[[str], None] = None) -> None:
     """
     目录文件对比
+    :param part_suffix: 参与比对的文件后缀
     :param root_path: 输入目录，所有文件以该目录为准
     :param target_path: 对比目录
     :param file_same_call: 文件相同回调，每有一个文件相同时就会回调
@@ -50,7 +118,7 @@ def path_compare(root_path: str, target_path: str, file_same_call: Callable[[str
             target_file_path = os.path.join(target_base_path, file)
 
             if os.path.isfile(target_file_path):
-                if cmp(root_file_path, target_file_path):
+                if compare_files(root_file_path, target_file_path, part_suffix):
                     file_same_call and file_same_call(root_file_path, target_file_path)
                 else:
                     file_diff_call and file_diff_call(root_file_path, target_file_path)
@@ -58,24 +126,33 @@ def path_compare(root_path: str, target_path: str, file_same_call: Callable[[str
                 file_miss_call and file_miss_call(root_file_path)
 
 
-# 使用方法
-dir1 = r"/Users/zhaojyu/Documents/MyUtils"
-dir2 = r"/Users/zhaojyu/Documents"
+if __name__ == "__main__":
+    root_path = r"D:\LoVN\client\main\resource"
+    target_path = r"D:\Lo\client\main\resource"
+    res_json_path = r"D:\LoVN\client\main\resource\default_cn.res.json"
 
-# # 创建多语言目录
-# localDir = dir1 + "\\cn"
-# os.path.exists(localDir) or os.mkdir(localDir)
-
-
-def diffCall(root_path, com_path)->None:
-    print("diff"+root_path,com_path)
-    # shutil.copy2(com_path, com_path);
-
-def missCall(root_path)->None:
-    print("miss"+root_path)
-
-# def nofindCall():
-#     pass
+    json_data = read_json_file(res_json_path)
 
 
-path_compare(dir1, dir2,file_diff_call=diffCall,file_miss_call=missCall)
+    def copy_change_res(root_file, target):
+        file_copy(root_file, target)
+        file_name_arr = os.path.basename(root_file).split('.')
+        relative_target_path = os.path.relpath(root_file, target_path)
+        for i in json_data["resources"]:
+            if i["name"] == f"{file_name_arr[-2]}_{file_name_arr[-1]}":
+                i["url"] = "cn/" + relative_target_path.replace('\\', '/')
+
+
+    def diffCall(root, comp) -> None:
+        print("diff" + root, comp)
+        relative_target_path = os.path.relpath(comp, target_path)
+        full_target_path = os.path.join(root_path, "cn" + os.sep + os.path.dirname(relative_target_path))
+        copy_change_res(comp, full_target_path)
+        same_json_file = Path(comp).with_suffix(".json")
+        if os.path.exists(same_json_file):
+            # 如果有同名的json文件一起移动过来
+            copy_change_res(str(same_json_file), full_target_path)
+
+
+    path_compare(root_path, target_path, part_suffix=["png", "json", "jpg"], file_diff_call=diffCall)
+    write_json_to_file(json_data, res_json_path)
